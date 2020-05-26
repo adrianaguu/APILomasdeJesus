a94465188f6439fca1f76c4dc468ff850dc277b9
adrianaguu <adriana.gutierrez@ucsp.edu.pe>
2020-05-26 11:24:38 -0500

    first commit

---
+++ contact/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    social_media = models.CharField(max_length=300)
+    hobbies = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
