d9165bc88540d7b67afb6092d86fd405aa941c99
adrianaguu <adriana.gutierrez@ucsp.edu.pe>
2020-06-11 13:10:49 -0500

    admin fix

--- contact/models.py
+++ contact/models.py
@@ -8,3 +8,10 @@ class Lead(models.Model):
     social_media = models.CharField(max_length=300)
     hobbies = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self): 
+        return self.name 
+
+    class Meta: 
+        verbose_name = 'Registrado'
