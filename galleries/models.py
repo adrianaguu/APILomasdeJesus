7130923c9259e52ad4db3253c53e1391ae2cfb74
adrianaguu <adriana.gutierrez@ucsp.edu.pe>
2020-08-06 11:00:10 -0500

    small image gallery

--- galleries/models.py
+++ galleries/models.py
@@ -14,7 +14,8 @@ class ImageGallery(models.Model):
 	name = models.CharField(max_length=100, unique=True, null=True)
 	gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE,verbose_name="Galería")
 	description = models.CharField(verbose_name="Descripción",max_length=150,blank=True,null=True)
-	urlImage = models.ImageField(verbose_name="Imagen",upload_to='images/galleries/')
+	urlImage = models.ImageField(verbose_name="Imagen grade",upload_to='images/galleries/')
+	urlImage_small = models.ImageField(verbose_name="Imagen pequeña",upload_to='images/galleries/',blank=True,null=True)
 
 	def __str__(self):
 		return self.name
--- galleries/serializers.py
+++ galleries/serializers.py
@@ -8,6 +8,7 @@ class GallerySerializer(serializers.ModelSerializer):
 
 class ImageGallerySerializer(serializers.ModelSerializer):
     urlImage = serializers.SerializerMethodField('get_image_url')
+    urlImage_small = serializers.SerializerMethodField('get_image_urls')
 
     class Meta:
         model = ImageGallery
@@ -15,3 +16,6 @@ class ImageGallerySerializer(serializers.ModelSerializer):
 
     def get_image_url(self, obj):
         return obj.urlImage.url
+
+    def get_image_urls(self, obj):
+        return obj.urlImage_small.url
